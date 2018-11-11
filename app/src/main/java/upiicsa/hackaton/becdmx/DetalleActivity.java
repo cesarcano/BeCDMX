2549b585a30ab148a2b46a9bb9c20ed6b737c8e2
César Cano <cesarcanojmz@gmail.com>
2018-11-11 09:07:44 -0600

    SASAADSZ

---
+++ app/src/main/java/upiicsa/hackaton/becdmx/DetalleActivity.java
@@ -0,0 +1,13 @@
+package upiicsa.hackaton.becdmx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+    }
+}
