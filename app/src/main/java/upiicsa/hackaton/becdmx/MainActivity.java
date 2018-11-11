5738341116252d9f63a1cbd03d10ebee41d60b29
César Cano <cesarcanojmz@gmail.com>
2018-11-11 02:46:19 -0600

    helper

--- app/src/main/java/upiicsa/hackaton/becdmx/MainActivity.java
+++ app/src/main/java/upiicsa/hackaton/becdmx/MainActivity.java
@@ -5,12 +5,13 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import upiicsa.hackaton.becdmx.helper.BaseActivity;
+
+public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
