60fb032bb52f5f58856f1e11a883b5dcf56b887d
César Cano <cesarcanojmz@gmail.com>
2018-11-11 10:41:25 -0600

    lOGIN

---
+++ app/src/main/java/upiicsa/hackaton/becdmx/LoginActivity.java
@@ -0,0 +1,29 @@
+package upiicsa.hackaton.becdmx;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.b_flogin)
+    Button fb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.b_flogin)
+    void fb() {
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
+    }
+}
